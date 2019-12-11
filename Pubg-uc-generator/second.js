6b952e27ba86d17bd8d91c853d63c444626a5d65
Clown Monster <mk6386223@gmail.com>
2019-12-11 18:15:28 +0530

    first addition

---
+++ Pubg-uc-generator/second.js
@@ -0,0 +1,27 @@
+function submain()
+{
+
+	alert("Successfully Generated UC and BP");
+}
+
+function fun2()
+{
+	var y =document.getElementById('dis1')
+	y.style.display = "block";
+}
+
+function fun3()
+{
+
+	var z = document.getElementById("share-buttons");
+	z.style.display = "block";
+}
+
+function fun7()
+{
+	document.getElementById("end-btn").onclick = "javascript:locaton.href='https://clownmonster.github.io' "
+	var e = document.getElementById("end-p");
+	e.style.display = "block";
+
+
+}
